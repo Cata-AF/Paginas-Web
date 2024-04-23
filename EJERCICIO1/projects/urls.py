3f080f551ba422bd9faa68998253e028743cd29d
Cata-AF <catiskura@gmail.com>
2024-04-22 22:43:36 -0500

    Templates y Admin Django

--- EJERCICIO1/projects/urls.py
+++ EJERCICIO1/projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.products, name="products"),
+    path('', views.products, name="products"),
     path('project/<str:pk>/', views.project, name="project")
 
 ]
--- EJERCICIO1/projects/views.py
+++ EJERCICIO1/projects/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {
+        'id':'1',
+        'title':"Ecommerce Website",
+        'description':'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':"Portafolio Website",
+        'description':'This was a project where I built out my portafolio'
+    
+    },
+    {
+        'id':'3',
+        'title':"Social Network",
+        'description':'Awesome open source project I am still working on'
+    },
+]
 
 def products(request):
-    return HttpResponse('Here are our products')
+    page = 'products'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'products.html', context)
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'single-project.html', {'projectObj':projectObj})
